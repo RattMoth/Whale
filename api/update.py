05e30d03f29ef20ba9efd9bafa6bb6042b6a5860
Chris McKenzie <kristopolous@yahoo.com>
2019-04-17 15:58:26 -0700

    tracking the broad market ETFs

--- api/update.py
+++ api/update.py
@@ -4,3 +4,18 @@ import lib
 lib.upgrade()
 lib.historical(["AAPL","MSFT","SNAP","FB","PEP","KO","TSLA","XOM","AMZN","WMT","NFLX","DIS","GOOG","BIDU","V","PYPL","PM","PLNT"])
 
+lib.historical([
+  # Goldman sachs
+  "GSLC","GSEW",
+  # Schwab broad market & large cap
+  "SCHB","SCHV",
+  # DJIA ETF by SPDR
+  "DIA",
+  # NASDAQ ETF by Fidelity 
+  "ONEQ",
+  # S&P 500 ETF by Vanguard & their broad market
+  "VOO", "VTI",
+  # Russell 2000 ETF by iShares 
+  "IWM"
+])
+
