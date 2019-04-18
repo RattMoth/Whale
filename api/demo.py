4cc18b9d304145e01b018b9f094af4154522bf6e
Chris McKenzie <kristopolous@yahoo.com>
2019-04-17 20:02:24 -0700

    cleaning up the demo app

--- api/demo.py
+++ api/demo.py
@@ -1,36 +1,23 @@
 #!/usr/bin/env python3
-from Robinhood import Robinhood
+import robin
 import redis
 import time
 import sys
 import json
-import pprint
 import sys
-import getpass
 import lib
 
 lib.upgrade()
 
-lib.get_yesterday('ticker, open, close')
-sys.exit(0)
-#[print('"{}","{}",'.format(y[0][0], y[1][0]), end='') for y in l]
 if len(sys.argv) < 2:
   print("Login username required")
   sys.exit(0)
-lib.login(sys.argv[1])
-lib.historical(["AAPL","MSFT","SNAP","FB","PEP","KO","TSLA","XOM","AMZN","WMT","NFLX","DIS","GOOG","BIDU","V","PYPL","PM","PLNT"])
+robin.login(sys.argv[1])
+robin.historical(["AAPL","MSFT","SNAP","FB","PEP","KO","TSLA","XOM","AMZN","WMT","NFLX","DIS","GOOG","BIDU","V","PYPL","PM","PLNT"])
 
-
-#analyze()
-#sys.exit(0)
 
 while True:
   print("> ", end="")
   cmd = input()
-  eval("lib." + cmd)()
+  eval("robin." + cmd)()
 
-#dividends()
-#portfolio()
-
-#portfolio()
-#pprint.pprint(my_trader.order_history())
