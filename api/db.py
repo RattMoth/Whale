226d0b92c0d07abdc1e67542eec8a1ee653f88cb
Chris McKenzie <kristopolous@yahoo.com>
2019-04-17 20:01:58 -0700

    Putting a primary key on the instruments table

--- api/db.py
+++ api/db.py
@@ -7,6 +7,7 @@ from threading import Lock
 user = {}
 _SCHEMA = {
   'instruments' : [
+    ('id', 'integer primary key autoincrement'), 
     ('ticker', 'text unique'),
     ('name', 'text'),
     ('description', 'text')
