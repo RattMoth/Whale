770eb91f51368c9d615af5ca7131f7ce73e329af
RattMoth <matt.dipaolo.roth@gmail.com>
2019-08-30 17:39:54 -0700

    Modify get function. Move background to z-index:-1
    When hosting locally (with `python3 -m http.server`) `document.location.host` returns the hostname along with the port number. The port number is removed from the hostname on line 229 using regex and the replace method

--- fe/web/js/stockstack.js
+++ fe/web/js/stockstack.js
@@ -1,84 +1,83 @@
-var 
-  Template = {},
-  trash = [], 
-  stash = [],
-  chooser,
-  Streak = 0,
-  ZeroTime = 3,
-  time_between = ZeroTime,
-  HistoricalMap = {},
-  Game = {
-    round: -1
-  },
-  last = {
-    lose: 0,
-    compliment: 0,
-    insult: 0
-  },
-  phrases = {
-    index: {},
-    loseList: [
-      "You drowned",
-      "Beached whale",
-      "We caught you",
-      "Go Fish!"
-    ],
-    complimentList: [
-      "That was easy",
-      "Finally that loser stopped playing",
-      "I'm really a catfish",
-      "You're krilling it",
-    ],
-    insultList: [
-      "Gee you're terrible at this!",
-      "I can do better and I don't have hands",
-      "Your finger must have slipped",
-      "Stop smoking the seaweed",
-      "My mom's a whale too",
-      "You suck like my blowhole",
-      "I wish I could drown"
-    ]
-  },  
-  ev = EvDa(),
-  CompanyList = [
-    [['AAPL', 'Apple'], ['MSFT', 'Microsoft']],
-    [['SNAP', 'Snapchat'], ['FB', 'Facebook']],
-    [['PEP', 'Pepsi'], ['KO', 'Coke']],
-    [['TSLA', 'Tesla'], ['XOM', 'Exxon Mobil']],
-    [['AMZN', 'Amazon'], ['WMT', 'Walmart']],
+const Template = {};
+let trash = [];
+let stash = [];
+let chooser;
+let Streak = 0;
+let ZeroTime = 3;
+let time_between = ZeroTime;
+let HistoricalMap = {};
+let Game = {
+  round: -1,
+};
+let last = {
+  lose: 0,
+  compliment: 0,
+  insult: 0,
+};
+let phrases = {
+  index: {},
+  loseList: [
+    'You drowned',
+    'Beached whale',
+    'We caught you',
+    'Go Fish!',
+  ],
+  complimentList: [
+    'That was easy',
+    'Finally that loser stopped playing',
+    "I'm really a catfish",
+    "You're krilling it",
+  ],
+  insultList: [
+    "Gee you're terrible at this!",
+    "I can do better and I don't have hands",
+    'Your finger must have slipped',
+    'Stop smoking the seaweed',
+    "My mom's a whale too",
+    'You suck like my blowhole',
+    'I wish I could drown',
+  ],
+};
+let ev = EvDa();
+let CompanyList = [
+  [['AAPL', 'Apple'], ['MSFT', 'Microsoft']],
+  [['SNAP', 'Snapchat'], ['FB', 'Facebook']],
+  [['PEP', 'Pepsi'], ['KO', 'Coke']],
+  [['TSLA', 'Tesla'], ['XOM', 'Exxon Mobil']],
+  [['AMZN', 'Amazon'], ['WMT', 'Walmart']],
 
-    // Maybe not disney?
-    [['NFLX', 'Netflix'], ['DIS', 'Disney']],
-    
-    // A better one for the goog would be nice.
-    [['GOOG', 'Google'], ['BIDU', 'Baidu']],
-    [['V', 'Visa'], ['PYPL', 'PayPal']],
-    [['PM', 'Philip Morris'], ['PLNT', 'Planet Fitness']]
-  ];
+  // Maybe not disney?
+  [['NFLX', 'Netflix'], ['DIS', 'Disney']],
+
+  // A better one for the goog would be nice.
+  [['GOOG', 'Google'], ['BIDU', 'Baidu']],
+  [['V', 'Visa'], ['PYPL', 'PayPal']],
+  [['PM', 'Philip Morris'], ['PLNT', 'Planet Fitness']],
+];
 
 function iter(what) {
-  phrases.index[what] = ( phrases.index[what] + 1 ) % phrases[what].length;
+  phrases.index[what] = (phrases.index[what] + 1) % phrases[what].length;
   return phrases[what][phrases.index[what]];
 }
 
 function render() {
-  if(Streak > 0) {
-    $("#win").append("<div>+1</div>");
-    $("#whale-speak").html(iter('complimentList'));
+  if (Streak > 0) {
+    $('#win').append('<div>+1</div>');
+    $('#whale-speak').html(iter('complimentList'));
   } else {
-    $("#lose").addClass('lose');
-    $("#whale-speak").html(iter('loseList'));
+    $('#lose').addClass('lose');
+    $('#whale-speak').html(iter('loseList'));
   }
 
-  $("#streak-container").html(Streak);
+  $('#streak-container').html(Streak);
 }
 
 function shuffle(array) {
-  var m = array.length, t, i;
+  let m = array.length; let t; let
+    i;
 
   // While there remain elements to shuffle…
   while (m) {
-
     // Pick a remaining element…
     i = Math.floor(Math.random() * m--);
 
@@ -92,13 +91,11 @@ function shuffle(array) {
 }
 
 function remove(tuple, set) {
-  if(!set) {
+  if (!set) {
     trash = remove(tuple, trash);
     stash = remove(tuple, stash);
   } else {
-    return set.filter(function(row) {
-      return row[0] !== tuple[0];
-    });
+    return set.filter((row) => row[0] !== tuple[0]);
   }
 }
 
@@ -109,7 +106,7 @@ function add(tuple, set) {
 
 function win() {
   time_between *= 0.9;
-  Streak ++;
+  Streak++;
 }
 function lose() {
   time_between = ZeroTime;
@@ -117,18 +114,18 @@ function lose() {
 }
 function pick(index, which) {
   // make sure they are out of the list
-  remove(CompanyList[index][0]); 
-  remove(CompanyList[index][1]); 
+  remove(CompanyList[index][0]);
+  remove(CompanyList[index][1]);
 
-  var 
-  stash_ix = which,
-    trash_ix = (which + 1) % 2;
+  const
+    stash_ix = which;
+  let trash_ix = (which + 1) % 2;
 
-  var 
-  chosen = CompanyList[index][stash_ix][0],
-    not_chosen = CompanyList[index][trash_ix][0];
+  const
+    chosen = CompanyList[index][stash_ix][0];
+  let not_chosen = CompanyList[index][trash_ix][0];
 
-  if(HistoricalMap[chosen] >  HistoricalMap[not_chosen]) {
+  if (HistoricalMap[chosen] > HistoricalMap[not_chosen]) {
     win();
   } else {
     lose();
@@ -136,49 +133,45 @@ function pick(index, which) {
 }
 
 function relative_percent(what) {
-  let rel = Math.round((1 - what) * 100 * 100);
+  const rel = Math.round((1 - what) * 100 * 100);
   let str = (rel / 100).toFixed(2);
-  if(rel > 0) {
-    str  = '+' + str;
+  if (rel > 0) {
+    str = `+${ str}`;
   }
-  return str + "%";
+  return `${str}%`;
 }
 
 function doPercent(what, percent) {
   percent += 1;
   console.log(what, percent, relative_percent(percent));
-  $(".performance." + what).html(relative_percent(percent));
+  $(`.performance.${  what}`).html(relative_percent(percent));
 }
 
 function endGame() {
-  $("#choice-container").html(
-    Template.endgame()
+  $('#choice-container').html(
+    Template.endgame(),
   );
   clearInterval(chooser);
-  $("#timer").hide();
-  let max = Math.max.apply(0, Object.values(HistoricalMap).map(a => a[2]));
-  let min = Math.min.apply(0, Object.values(HistoricalMap).map(a => a[2]));
+  $('#timer').hide();
+  const max = Math.max.apply(0, Object.values(HistoricalMap).map(a => a[2]));
+  const min = Math.min.apply(0, Object.values(HistoricalMap).map(a => a[2]));
 
-  for(var ticker in HistoricalMap) {
-    let change = HistoricalMap[ticker][2];
-    let perc = 100 * (max - change) / (max - min);
-    $(".performance." + ticker).html(Math.round(perc/10));
-    HistoricalMap[ticker][3] = Math.round(perc/10);
+  for (const ticker in HistoricalMap) {
+    const change = HistoricalMap[ticker][2];
+    const perc = 100 * (max - change) / (max - min);
+    $(`.performance.${  ticker}`).html(Math.round(perc / 10));
+    HistoricalMap[ticker][3] = Math.round(perc / 10);
 
     console.log(change, perc, max, min);
-    $(".perf-container." + ticker + ' .waves').css('height', perc + '%');
-    $(".perf-container." + ticker + ' .performance').css('bottom', .25 + .5 * perc + '%');
+    $(`.perf-container.${  ticker  } .waves`).css('height', `${perc }%`);
+    $(`.perf-container.${  ticker  } .performance`).css('bottom', 0`${0.25 + 0.5 * perc}%`);
   }
 
-  let stashTotal = stash.reduce(function(ix, row) {
-    return HistoricalMap[row[0]][3] + ix;
-  }, 0);
-  let trashTotal = trash.reduce(function(ix, row) {
-    return HistoricalMap[row[0]][3] + ix;
-  }, 0);
-  $(".performance.stash").html(stashTotal);
-  $(".performance.trash").html(trashTotal);
-  $(".performance.final").html(stashTotal - trashTotal);
+  const stashTotal = stash.reduce((ix, row) => HistoricalMap[row[0]][3] + ix, 0);
+  const trashTotal = trash.reduce((ix, row) => HistoricalMap[row[0]][3] + ix, 0);
+  $('.performance.stash').html(stashTotal);
+  $('.performance.trash').html(trashTotal);
+  $('.performance.final').html(stashTotal - trashTotal);
   Game.over = true;
   return false;
 }
@@ -190,24 +183,24 @@ function choose(ix, which) {
 }
 
 function nextRound() {
-  if(Game.over) { 
+  if (Game.over) {
     return false;
   }
   Game.round += 1;
 
-  if(Game.round >= CompanyList.length) {
+  if (Game.round >= CompanyList.length) {
     return endGame();
   }
 
-  var timer_ix = parseFloat(time_between.toFixed(1));
-  if(chooser) {
+  let timer_ix = parseFloat(time_between.toFixed(1));
+  if (chooser) {
     clearInterval(chooser);
   }
   $('#counter').html(timer_ix);
-  chooser = setInterval(function(){
-    timer_ix --;
+  chooser = setInterval(() => {
+    timer_ix--;
     $('#counter').html(timer_ix.toFixed(1));
-    if(timer_ix < 0) {
+    if (timer_ix < 0) {
       clearInterval(chooser);
       lose();
       render();
@@ -215,38 +208,41 @@ function nextRound() {
     }
   }, 1000);
 
-  $("#choice-container").html(
-    Template.choice({ix: Game.round, choice: CompanyList[Game.round] })
+  $('#choice-container').html(
+    Template.choice({ ix: Game.round, choice: CompanyList[Game.round] }),
   );
   return true;
 }
 
 function loadTemplates() {
-  $("#template > *").each(function(){
-    var id = this.id.slice(2);
+  $('#template > *').each(function () {
+    const id = this.id.slice(2);
     Template[id] = _.template(this.innerHTML);
-    console.log(">> template " + id);
+    console.log(`>> template ${  id}`);
   });
 }
 
 function get(url, cb) {
-  var http = new XMLHttpRequest();
+  const http = new XMLHttpRequest();
 
-  http.open('GET', 'http://' + document.location.host + ':4001/' + url, true);
+  // Gets hostname and removes port
+  const hostname = document.location.host.replace(/:\d{4}/, '');
+
+  http.open('GET', `http://${hostname}:4001/${url}`, true);
   http.setRequestHeader('Content-type', 'application/json');
 
-  http.onreadystatechange = function() {
-    if(http.readyState == 4 && http.status == 200) {
+  http.onreadystatechange = function () {
+    if (http.readyState == 4 && http.status == 200) {
       cb(http.responseText);
     }
-  }
+  };
   http.send();
 }
 
 function getYesterday() {
-  get('yesterday', function(data) {
-    var res = JSON.parse(data);
-    res.data.forEach(function(row) {
+  get('yesterday', (data) => {
+    let res = JSON.parse(data);
+    res.data.forEach((row) => {
       let openClose = row.slice(1);
       openClose.push(openClose[1] / openClose[0]);
       HistoricalMap[row[0]] = openClose;
@@ -256,17 +252,16 @@ function getYesterday() {
 }
 
 function shufflePhrases() {
-  for(var x in phrases) {
+  for (const x in phrases) {
     phrases[x] = shuffle(phrases[x]);
     phrases.index[x] = 0;
   }
 }
 
-$(function(){
+$(() => {
   getYesterday();
   loadTemplates();
   shufflePhrases();
   nextRound();
   render();
 });
-
