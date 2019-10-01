2038d017a700d4d486e23ce97622e45ea46404c1
Chris McKenzie <kristopolous@yahoo.com>
2019-10-01 00:44:14 -0700

    fixing the db install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install redis-server sqlite3
-[[ -e trades.db ]] || gunzip -c trades.db.gz 
+[[ -e trades.db ]] || gunzip trades.db.gz 
 cd api
 pip3 install -r requirements.txt
 
