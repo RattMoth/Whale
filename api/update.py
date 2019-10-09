968a79135710925a45ba2bfdfaffd298628476bc
Chris McKenzie <kristopolous@yahoo.com>
2019-10-09 12:23:25 -0700

    adding a bunch of stocks

--- api/update.py
+++ api/update.py
@@ -3,25 +3,25 @@ import lib
 import alpha
 
 lib.upgrade()
-alpha.historical([
-  "BAC", "ARCH",
-  "F", "SPLS",
-  "GM", "FTR",
-  "WFC", "HRB",
-  "V", "T",
-  "VZ", "CTL",
-  "TWTR", "MAT",
-  "GE", "ULTA",
-  "GRPN", "LULU",
-  "PFE", "DPZ",
-  "CVX", "PCG",
-  "JPM", "HCC",
-  "JNJ", "FSLR",
-  "MMM",
-  "UBER",
-  "SFIX",
+alpha.historical(list(set([
+  "BAC", "ARCH", "S", "ORCL", "BBY",
+  "F", "SPLS", "SIRI", "SQ", "CL"
+  "GM", "FTR", "BABA", "NKE", "GIS",
+  "WFC", "HRB", "PLUG", "GS", "LVS",
+  "V", "T", "LLY", "TWLO", "KSS",
+  "VZ", "CTL", "QCOM", "MS", "ETSY",
+  "TWTR", "MAT", "ROKU", "AMGN", "AAL",
+  "GE", "ULTA", "INTC", "LOW", "FL",
+  "GRPN", "LULU", "AMD", "KHC", "MGM",
+  "PFE", "DPZ", "NVDA", "AXP", "M",
+  "CVX", "PCG", "C", "ATVI", "KR",
+  "JPM", "HCC", "HD", "EBAY", "HAL",
+  "JNJ", "FSLR", "DG", "DAL", "ACB",
+  "MMM", "FIT", "PG", "ZM", "HPQ",
+  "UBER", "GPRO", "ADBE", "TMUS",
+  "SFIX", "ZNGA", "CVS", "LUV",
 
-  "AAPL","MSFT","SNAP","FB","PEP","KO","TSLA","XOM","AMZN","WMT","NFLX","DIS","GOOG","BIDU","V","PYPL","PM","PLNT"])
+  "AAPL","MSFT","SNAP","FB","PEP","KO","TSLA","XOM","AMZN","WMT","NFLX","DIS","GOOG","BIDU","V","PYPL","PM","PLNT"])))
 
 alpha.historical([
   # Goldman sachs
